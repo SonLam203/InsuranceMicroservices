30a8d9dc5117413bd0df647e4da9a23f3ab315e6
Sơn Lâm <lamvnpro2005@gmail.com>
2026-03-17 13:00:08 +0700

    bổ sung

--- PolicyService.Tests/obj/Debug/net8.0/PolicyService.Tests.AssemblyInfo.cs
+++ PolicyService.Tests/obj/Debug/net8.0/PolicyService.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PolicyService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843292943ca442ab2146db71288b9bf0d204867c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PolicyService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PolicyService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
